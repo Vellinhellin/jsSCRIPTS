46191ecda04028582394ff9779a6b5dffde77548
Vellinhellin <veniaminloza@gmail.com>
2026-04-16 20:38:11 +0300

    add karysel

--- scripts.js
+++ scripts.js
@@ -1,5 +1,42 @@
-{let a = +prompt("Введите свой возраст", "");
-let b = Number(10)
-let c = a + b
-console.log(c)
+//массив с контейнерами
+const containers = [
+    document.querySelector('#first'),
+    document.querySelector('#second'),
+    document.querySelector('#third')
+];
+
+const vperBtn = document.querySelector('#vperBtn');
+const nazBtn = document.querySelector('#nazBtn');
+
+let schet = 1; //начинаем со второго контейнера
+
+//функция показа контейнера
+function pokazKont(index) {
+    containers[0].classList.add('hidden');
+    containers[1].classList.add('hidden');
+    containers[2].classList.add('hidden');
+    containers[index].classList.remove('hidden');
+    schet = index;
 }
+
+//функция для кнопки Назад
+function nazad() {
+    if (schet === 0) {
+        pokazKont(2);
+    } else {
+        pokazKont(schet - 1);
+    }
+}
+
+//функция для кнопки Вперёд
+function vpered() {
+    if (schet === 2) {
+        pokazKont(0);
+    } else {
+        pokazKont(schet + 1);
+    }
+}
+
+//привязываем именные функции к кнопкам
+vperBtn.addEventListener('click', nazad);
+nazBtn.addEventListener('click', vpered);
--- test.js
+++
@@ -1,3 +0,0 @@
-function myfunc(a, b){return a + b}
-let result = myfunc(3,2)
-console.log(result)
